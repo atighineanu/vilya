aff221a0fd94155a481b2c4c74d607ff3a6769ec
atighineanu <atighineanu@suse.de>
2020-11-18 18:52:56 +0100

    added search func

--- cmd/search.go
+++ cmd/search.go
@@ -16,9 +16,10 @@ limitations under the License.
 package cmd
 
 import (
+	"log"
 	"fmt"
-	"vilya/pkg/utils"
 	//"vilya/pkg/utils"
+
 	"github.com/spf13/cobra"
 )
 
@@ -26,7 +27,7 @@ import (
 var (
  searchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
+	Short: "searches for maintenane updates affecting the product",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -34,8 +35,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("search called")
-		utils.CheckForUpd(Config)
+		Search()
 	},
 }
 team string
@@ -44,9 +44,19 @@ team string
 
 func init() {
 	rootCmd.AddCommand(searchCmd)
-	rootCmd.PersistentFlags().StringVar(&team, "team", "default", "what is the abrev. of the team in maintenance qam-caasp/qam-sle/qam-suma")
+	rootCmd.PersistentFlags().StringVar(&team, "team", "qam-manager", "what is the abrev. of the team in maintenance qam-caasp/qam-sle/qam-suma")
 	//rootCmd.PersistentFlags().IntVar(&workers, "workers", 0, "number of workers in the cluster")
 	//rootCmd.PersistentFlags().IntVar(&masters, "masters", 0, "number of masters in the cluster")
 	//rootCmd.PersistentFlags().StringVarP(&pool, "pool", "p", "", "name of pool for the project")
 	//rootCmd.PersistentFlags().StringVarP(&pool, "distro", "d", "", "name of distro in the cluster")
+}
+
+func Search() {
+	updates, err := Config.CheckForUpd()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	for _, elem := range updates {
+		log.Printf("Package: %+v  Repos: %+v\n", elem.SRCRPMS, elem.Repository)
+	}
 }
